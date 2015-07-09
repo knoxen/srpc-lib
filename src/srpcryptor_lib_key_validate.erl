65e5d79197d4387d57c8ade6627e8f91e152897f
dingo sky <paul@dingosky.com>
2015-07-09 14:05:45 -0700

    Option to specify hmac key for lib key validation

--- src/srpcryptor_lib_key_validate.erl
+++ src/srpcryptor_lib_key_validate.erl
@@ -10,7 +10,6 @@
 -define(LIB_KEY_ID_LEN,  12).
 -define(LIB_KEY_BYTES,   32).
 -define(CHALLENGE_BYTES, 32).
--define(EPOCH_BITS,      32).
 
 packet_data(KeyInfo, ValidatePacket) ->
   case srpcryptor_encryptor:decrypt(KeyInfo, ValidatePacket) of
@@ -22,15 +21,36 @@ packet_data(KeyInfo, ValidatePacket) ->
       Error
   end.
 
+
 response_packet({KeyId, KeyData}, ClientChallenge, RespData) ->
+  {LibKey, ValidateResult, LibRespData} = lib_info_for_key_data(KeyData, ClientChallenge, RespData),
+  KeyInfo = {KeyId, LibKey},
+  encrypt_response(KeyInfo, ValidateResult, LibRespData);
+response_packet({KeyId, KeyData, HmacKey}, ClientChallenge, RespData) ->
+  {LibKey, ValidateResult, LibRespData} = lib_info_for_key_data(KeyData, ClientChallenge, RespData),
+  KeyInfo = {KeyId, LibKey, HmacKey},
+  encrypt_response(KeyInfo, ValidateResult, LibRespData).
+
+lib_info_for_key_data(KeyData, ClientChallenge, RespData) ->
   {ValidateResult, ServerChallenge} = srpcryptor_srp:validate_challenge(KeyData, ClientChallenge),
-  EpochSeconds = srpcryptor_util:epoch_seconds(),
-  ProData = <<ServerChallenge/binary, EpochSeconds:?EPOCH_BITS>>,
-  ProRespData = <<ProData/binary, RespData/binary>>,
-  Key = maps:get(key, KeyData),
-  case srpcryptor_encryptor:encrypt({KeyId, Key}, ProRespData) of
+  LibRespData = <<ServerChallenge/binary, RespData/binary>>,
+  LibKey = maps:get(key, KeyData),
+  {LibKey, ValidateResult, LibRespData}.
+
+encrypt_response(KeyInfo, ValidateResult, LibRespData) ->
+  case srpcryptor_encryptor:encrypt(KeyInfo, LibRespData) of
     {ok, RespPacket} ->
       {ValidateResult, RespPacket};
     Error ->
       Error
   end.
+
+%% response_packet(KeyInfo, ClientChallenge, RespData) ->
+%%   {LibKey, LibRespData} = lib_info_for_key_data(KeyInfo, ClientChallenge, RespData),
+%%   case srpcryptor_encryptor:encrypt({KeyId, LibKey}, LibRespData) of
+%%     {ok, RespPacket} ->
+%%       {ValidateResult, RespPacket};
+%%     Error ->
+%%       Error
+%%   end.
+
