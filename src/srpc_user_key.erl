cf3799546fab9409154fc1e7afe19da3a0ef5b1e
dingo sky <paul@dingosky.com>
2015-11-30 12:25:40 -0800

    Client type names

--- src/srpc_user_key.erl
+++ src/srpc_user_key.erl
@@ -69,7 +69,7 @@ create_exchange_response(CryptClientMap, SrpcUserData, ClientPublicKey, Exchange
                              KdfSalt, SrpSalt, ServerPublicKey, ExchangeData) of
     {ok, {ClientId, ExchangeResponse}} ->
       ClientMap = srpc_srp:client_map(ClientId, ClientPublicKey, ServerKeys, SrpValue),
-      ExchangeMap = maps:merge(ClientMap, #{clientType => user_client
+      ExchangeMap = maps:merge(ClientMap, #{clientType => srpc_user_client
                                            ,entityId   => UserId}),
       {ok, {ExchangeMap, ExchangeResponse}};
     Error ->
